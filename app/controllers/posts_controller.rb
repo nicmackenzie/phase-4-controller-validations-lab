9104ff929a488fe6ae72c170158bde5a45497923
nicmackenzie <nicmackenzie@live.com>
2023-08-17 15:01:10 +0300

    completed lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid_record_errors
 
   def show
     post = Post.find(params[:id])
@@ -9,7 +10,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +19,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def handle_invalid_record_errors(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates_inclusion_of :category, in: ['Fiction', 'Non-Fiction']
+    validates :content, { length: { minimum: 100 } }
 end
